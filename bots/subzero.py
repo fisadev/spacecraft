8f3927140d4b891e6e2e699cc98240fbecbad77e
Juan Pedro Fisanotti <fisadev@gmail.com>
2012-07-09 22:12:48 -0300

    Get correct bot filename to freeze it

--- bots/subzero.py
+++ bots/subzero.py
@@ -1,5 +1,7 @@
 #-*- coding: utf-8 *-*
 import os
+import sys
+import inspect
 
 from twisted.internet import reactor
 from twisted.internet.protocol import ClientFactory
@@ -9,6 +11,10 @@ from spacecraft.bots.fisa import FisaBotClient
 
 class SubZeroBotClient(FisaBotClient):
     name = 'SubZero'
+
+    def __init__(self):
+        super(SubZeroBotClient, self).__init__()
+        self.bot_files = get_bot_files_by_name()
 
     def messageReceived(self, msg):
         super(SubZeroBotClient, self).messageReceived(msg)
@@ -22,11 +28,32 @@ class SubZeroBotClient(FisaBotClient):
                     ps = os.popen('ps fx').read()
                     p_id = [l.strip().split()[0]
                             for l in ps.split('\n')
-                            if 'python bots/%s' % e.name in l]
+                            if 'python bots/%s' % self.bot_files[e.name] in l]
                     if p_id:
                         os.popen('kill -STOP %s' % p_id[0]).read()
                 except:
                     pass
+
+
+def get_bot_files_by_name():
+    source_dir, myfile = os.path.split(__file__)
+    sys.path.append(os.path.join(os.getcwd(), source_dir))
+    bots = {}
+    for sfile in os.listdir(source_dir):
+        if sfile.endswith("pyc"):
+            continue
+        head, tail = os.path.split(sfile)
+        if myfile == tail:
+            continue
+        module = __import__(os.path.splitext(tail)[0])
+        for name, value in inspect.getmembers(module):
+            if name == "ClientBase":
+                continue
+            elif getattr(value, 'name', None) is not None and \
+               inspect.isclass(value):
+                bots[value.name] = tail
+    return bots
+
 
 def main():
     factory = ClientFactory()
